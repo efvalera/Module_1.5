8d42b0d8c770c843243ecd472febc72b572dc3bb
efvalera <efvalera2014@gmail.com>
2024-08-01 08:22:44 +1000

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,5 @@
+immutable_var = 1, 2, 3, True, "string"
+print(immutable_var)
+immutable_var [0] = 3
+print(immutable_var) # кортеж относится к неизменяемому типу данных. Элементы кортежа не изменяются.
+
---
+++ module_1_5_2.py
@@ -0,0 +1,4 @@
+mutable_list = [5,6, "af", True]
+print (mutable_list)
+mutable_list [1] = 8
+print(mutable_list)
